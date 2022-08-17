f489c2305e89a638417b72f7c9b5564826a8155d
samiulislam09 <samiulislamsawon09@gmail.com>
2022-08-17 20:03:37 +0600

    added crud functionality

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
-const dotenv = require('dotenv');
-const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
-dotenv.config()
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
-
-const uri = process.env.MONGO_URL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-const run = async () => {
-    try {
-        await client.connect();
-        const collection = client.db("sagesource-blogs").collection("blogs");
-        const user = { name: 'samiul', email: "sawon@gmail.com" };
-        const result = await collection.insertOne(user)
-    } finally {
-        // await client.close();
-    }
-}
-
-run().catch(console.dir)
-
-app.get('/', (req, res) => {
-    res.send('Running app')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
 });
 
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
-//create post 
+app.use("/posts", postRoute);
+app.use("/categories", categoryRoute);
 
-app.listen(port, () => {
-    console.log('running')
-})
+app.listen("5000", () => {
+    console.log("Backend is running.");
+});
